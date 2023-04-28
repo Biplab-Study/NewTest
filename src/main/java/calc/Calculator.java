d274cbcf068ca5d7c2184974c07233707f612d43
Biplab-Study <130243249+Biplab-Study@users.noreply.github.com>
2023-04-28 16:32:18 +0530

    Update Calculator.java

--- src/main/java/calc/Calculator.java
+++ src/main/java/calc/Calculator.java
@@ -22,10 +22,10 @@ public class Calculator {
 	public static void main(String[] args) {
 		Calculator calc=new Calculator();
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Input the 1st number : ");
+		System.out.println("Input the 1st number");
 		int num1 = scan.nextInt();
 		
-		System.out.println("Input the 2nd number : ");
+		System.out.println("Input the 2nd number");
 		int num2 = scan.nextInt();
 		
 		scan.close();
